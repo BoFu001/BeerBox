bcf0a903bbed3c486f373051a49acd16c188b168
Bo Fu <bofu2013@gmail.com>
2023-02-20 01:31:40 +0100

    created a navigation bar

--- app/src/main/java/com/bofu/beerbox/MainActivity.kt
+++ app/src/main/java/com/bofu/beerbox/MainActivity.kt
@@ -2,10 +2,27 @@ package com.bofu.beerbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bofu.beerbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private val TAG = javaClass.simpleName
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        uiSetup()
+    }
+
+
+    private fun uiSetup(){
+
+        supportActionBar?.displayOptions = androidx.appcompat.app.ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.navigation_bar)
     }
 }
